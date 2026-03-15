8a8c6bd088c5102793f20912c205615677852e4e
SMarquize <smithmarquize@gmail.com>
2026-03-14 21:51:45 -0700

    hour7_part1

---
+++ MSmith_Hour7_P1/Assets/Script/Addition.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Addition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a;
+        a = 2;
+        int b;
+        b = 4;
+        int c;
+        c = 8;
+        int sum;
+        sum = a+c+b; ++sum;
+        Debug.Log(sum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
