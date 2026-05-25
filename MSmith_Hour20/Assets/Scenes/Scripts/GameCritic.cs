87e4c8be78d8f5d236e77627b896dc4fd265c475
SMarquize <smithmarquize@gmail.com>
2026-05-24 23:20:39 -0700

    Hour 20

---
+++ MSmith_Hour20/Assets/Scenes/Scripts/GameCritic.cs
@@ -0,0 +1,17 @@
+/*
+Is the game too easy or hard?
+the game was not easy but not that hard do to the randomness 
+
+What changes would you do to make it easier or harder?
+make the speed of the power up slower to let players get it or extend the timer
+
+What would make the game more appealing?
+make the envirment lighter
+
+What parts of the game are fun?
+fun part is trying to get the power up to extend the time
+
+What parts of the game are tedious?
+trying to get the power up
+
+ */
