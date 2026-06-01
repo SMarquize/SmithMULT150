85166e56a9dc6993cf2fa9e6a181bd91709dc63f
SMarquize <smithmarquize@gmail.com>
2026-06-01 02:15:08 -0700

    Final

---
+++ MSmith_Final/Assets/Scripts/Scripts/Modifications.cs
@@ -0,0 +1,48 @@
+public class Modifications
+{/* ================================================================
+AMAZING RACER
+================================================================
+1 - directional light (colour, intensity, etc)
+modifications:made the light light blue
+2 - fog
+modifications:remove it
+3 - create & apply skybox and customise it (atmosphere thickness, sky tint,
+exposure, etc)
+modifications: added a winter image
+
+PLAYER
+4 - create and attach a complex object comprised of 3 or more shapes and
+make it so that it is visible when the game is played.
+-If you're having trouble coming up with ideas, create a simple figure out
+of shapes like a Minecraft or Roblox character!
+modifications:made a door to the finisline 
+5 - use gameobject hierarchy to keep it organised
+[no record necessary]
+
+FINISH ZONE
+6 - convert to prefab, create 2 variants, and add an instance of each
+variant around the scene
+modifications:chage color and add a door
+7 - modify the light component of the variants so that all have a different
+
+colour
+[no record necessary]
+8 - add a particle system component to the original prefab and customise at
+least 3 parameters
+-All instances will update to include the particle system
+-Don't forget to reassign any public fields on the instances to get them to
+work
+parameter 1:Snow fall
+parameter 2:sparkle
+parameter 3:fire place
+
+TERRAIN
+9 - raise/lower to reshape and make it distinctly different from original
+modifications: rise the terrein to look like mountains and added spikes 
+10 - paint with at least 2 new textures to replace all the textures used in
+the book
+(add source url or package name)
+texture 1 source:ice
+texture 2 source:snow
+*/
+}
